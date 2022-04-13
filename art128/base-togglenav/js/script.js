449473eda2121bf89bb6fd4a8c5ecd5b47402ad1
kalebmiranda <97697576+kalebmiranda@users.noreply.github.com>
2022-04-12 23:39:30 -1000

    fixed by footer links and toggle nav

--- art128/base-togglenav/js/script.js
+++ art128/base-togglenav/js/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     //TOGGLE NAV MOBILE MENU FOR SMALL SCREENS
-    const menubutton = document.querySelector('.menu-botton');
+    const menubutton = document.querySelector('.menu-button');
     const menunav = document.querySelector('.toggle-nav');
     menubutton.addEventListener("click", function(){
         if (menunav.getAttribute('data-navstate') === 'open') {
